f4aaace1ba74c717ab69817d965e097e66fa9994
Essa Mehdi <Essa@Oswahs-MacBook-Air.local>
2026-03-03 21:03:38 +1100

    skeleton wrapper for coursepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
-// import CoursePage from './pages/CoursePage';
+import CoursePage from './pages/CoursePage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DashboardPage />} />
-        {/* <Route path="/course/:courseId" element={<CoursePage />} /> */}
+        <Route path="/course/:courseId" element={<CoursePage />} />
       </Routes>
     </BrowserRouter>
   )
