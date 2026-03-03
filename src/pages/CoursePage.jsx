8c070bffde033232c85d88d2e3948ea5553f1c67
Essa Mehdi <Essa@Oswahs-MacBook-Air.local>
2026-03-03 21:11:37 +1100

    sample coursepage reading from coursedata.js

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -1,7 +1,27 @@
+import { useParams } from 'react-router-dom';
+import { courses } from '../data/courseData';
+
 export default function CoursePage() {
-    return(
+    const { courseId } = useParams();
+    const course = courses[courseId];
+
+    if (!course) {
+        return (
+            <div>
+                <h1>Error! Course not found</h1>
+            </div>
+        )
+    }
+
+    return (
         <div>
-            <h1>CoursePage</h1>
+            <h1>{course.name}</h1>
+            <p>Faculty: {course.faculty}</p>
+            <p>Tutorials: {course.tutorials}</p>
+            <p>Labs: {course.labs}</p>
+            <p>Lectures: {course.lectures}</p>
+            <p>Assignments: {course.assignments}</p>
+            <p>Exams: {course.exams}</p>
         </div>
     )
 }
