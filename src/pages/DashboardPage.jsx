85106cbc977d39c80155b10fec8a642535ce0129
Essa Mehdi <Essa@Oswahs-MacBook-Air.local>
2026-03-03 21:28:00 +1100

    added buttons to navigate to each coursepage for a users selected courses (hardcoded courses for now)

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function DashboardPage() {
+    const navigate = useNavigate();
+    const courses = ["COMP2041", "COMP1511"];
+
     return(
         <div>
             <h1>DashboardPage</h1>
+            {courses.map((course) => (
+                <div key={course} onClick={() => navigate(`/course/${course}`)}>
+                    {course}
+                </div>
+            ))}
         </div>
     )
-}
+}   
