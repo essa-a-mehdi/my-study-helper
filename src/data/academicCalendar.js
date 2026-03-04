384028810a31ca06ce42a153a91a0a25e8027bc9
Essa Mehdi <Essa@Oswahs-MacBook-Air.local>
2026-03-04 19:19:44 +1100

    added a helper function to return the current week from academic calendar

---
+++ src/data/academicCalendar.js
@@ -0,0 +1,4 @@
+export const terms = {
+  "2026-T1": { startDate: "2026-02-16`", endDate: "2026-05-16" },
+  "2026-T2": { startDate: "2026-05-21", endDate: "2026-08-22" },
+}
---
+++ src/utils/dateUtils.jsx
@@ -0,0 +1,17 @@
+import terms from "../data/academicCalendar";
+
+export function getCurrentWeek(startDate) {
+    // get date right now
+    const currentDate = new Date();
+    // get the start date of this semester from academic calendar
+    const start = new Date(startDate);  
+    // diff in time : so this is milliseconds
+    const diffTime = currentDate - start;
+    // convert ms to day : 1 day = 24 hours * 60 minutes * 60 seconds * 1000 ms
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    // convert day to week : 1 week = 7 days
+    const diffWeeks = Math.floor(diffDays / 7);
+    // cos week 0
+    return diffWeeks + 1;
+
+}
